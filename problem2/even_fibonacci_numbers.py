58377927423f4a5ed910fa6e0affa787299e8519
Razeen Shaikh <razeen9796@outlook.com>
2022-06-23 16:49:40 +0530

    Even Fibonacci numbers solution

--- problem2/even_fibonacci_numbers.py
+++ problem2/even_fibonacci_numbers.py
@@ -1,12 +1,17 @@
-def fibonacci(n):
-    if n < 0:
-        print("Incorrect input")
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+# initial fibonacci numbers a and b
+a = 1
+b = 1
 
+n = 50000
+sum = 0
+for i in range(2, n):
+    fib = a + b
+    # making sure fibonacci number doesn't exceed 4 million
+    if fib > 4000000:
+        break
+    # Checking if the fibonacci number is even
+    if fib % 2 == 0:
+        sum += fib
+    a, b = b, fib
 
-print(fibonacci(4000000))
+print(sum)
